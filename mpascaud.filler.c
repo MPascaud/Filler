20bb4e76627b48faf50c9d98728c24ed82c5174f
Martin marie christian PASCAUD <mpascaud@e3r4p2.42.fr>
2018-03-11 01:29:21 +0100

    filler

--- mpascaud.filler.c
+++ mpascaud.filler.c
@@ -6,7 +6,7 @@
 /*   By: mpascaud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/08 16:38:30 by mpascaud          #+#    #+#             */
-/*   Updated: 2018/03/10 22:32:42 by mpascaud         ###   ########.fr       */
+/*   Updated: 2018/03/11 01:28:36 by mpascaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -194,9 +194,104 @@ void	ft_afficher_variables(t_data *data, char *tmp, int war, int shapiece)
 
 void	ft_heatmap(t_data *data)
 {
+	int		line;
 	int		i;
+	int		change;
+	int		heat;
 
+	line = 0;
 	i = 0;
+	change = 0;//inutile
+	heat = 9;
+	while (data->war[line] != NULL)
+	{
+		while (data->war[line][i])
+		{
+			if (data->war[line][i] == '.')
+				data->war[line][i] = '0';
+			i++;
+		}
+		i = 0;
+		line++;
+	}
+	i = 0; //inutile
+	line = 0;
+	while (data->war[line] != NULL)
+	{
+		while (data->war[line][i])
+		{
+			if (data->war[line][i] == data->opp)
+			{
+				change = i;
+				while (change >= 0 && heat >= 0)
+				{
+					if (data->war[line][change] != data->me && data->war[line][change] != data->opp 
+							&& data->war[line][change] < heat + '0')
+					{
+						data->war[line][change] = heat + '0';
+					}
+					heat--;
+					change--;
+				}
+				change = i;
+				heat = 9;
+				while (data->war[line][change] && heat >= 0)
+				{
+					if (data->war[line][change] != data->me && data->war[line][change] != data->opp
+							&& data->war[line][change] < heat + '0')
+					{
+						data->war[line][change] = heat + '0';
+					}
+					heat--;
+					change++;
+				}
+			}
+			i++;
+		}
+		i = 0;
+		heat = 9;
+		line++;
+	}
+	line = 0;
+	i = 0;
+	heat = 9;
+	while (data->war[line] != NULL)
+	{
+		while (data->war[line][i])
+		{
+			if (data->war[line][i] == data->opp)
+			{
+				change = line;
+				while (change >= 0 && heat >= 0)
+				{
+					if (data->war[change][i] != data->me && data->war[change][i] != data->opp
+							&& data->war[change][i] < heat + '0')
+					{
+						data->war[change][i] = heat + '0';
+					}
+					heat--;
+					change--;
+				}
+				change = line;
+				heat = 9;
+				while (change < data->board[0] && heat >= 0)
+				{
+					if (data->war[change][i] != data->me && data->war[change][i] != data->opp
+							&& data->war[change][i] < heat + '0')
+					{
+						data->war[change][i] = heat + '0';
+					}
+					heat--;
+					change++;
+				}
+			}
+			i++;
+			heat = 9;
+		}
+		i = 0;
+		line++;
+		//heat = 9;
+	}
 }
 
 int		main(void)
@@ -270,11 +365,14 @@ int		main(void)
 				}
 			}
 		}
-//		write(1, "8 2\n", 4);
-		write(1, "12 14\n", 6);
-		ft_heatmap(data);
 		//ft_filler(data);
 		//si on ne peut plus poser, break ici aussi
+	//	write(1, "0 0\n", 4);
+	//	write(1, "8 2\n", 4);
+		write(1, "12 14\n", 6);
+		if (tmp[0] == '=')
+			break ;
+		ft_heatmap(data);
 		ft_afficher_variables(data, tmp, war, shapiece);
 		ft_free(&data, &war, &shapiece);
 	}
